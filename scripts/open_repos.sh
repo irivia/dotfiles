e3179383a5b69789ebb388d2711a93d676bb55c1
Alucard <fire0lord0zuko@proton.me>
2026-06-06 06:31:15 +0300

    Update to use the PROJECTS_PATH environment variable

--- scripts/open_repos.sh
+++ scripts/open_repos.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-PROJECTS="$HOME/programming/repos/"
-
-selected=$(find "$PROJECTS" -mindepth 1 -maxdepth 1 -type d -exec basename {} \; | fzf --bind 'enter:accept-or-print-query')
+selected=$(find "$PROJECTS_PATH/" -mindepth 1 -maxdepth 1 -type d -exec basename {} \; | fzf --bind 'enter:accept-or-print-query')
 
 if [[ -n "$selected" ]]; then
-    if [[ ! -d "$PROJECTS/$selected" ]]; then
-        mkdir -p "$PROJECTS/$selected"
+    if [[ ! -d "$PROJECTS_PATH/$selected" ]]; then
+        mkdir -p "$PROJECTS_PATH/$selected"
     fi
-    tmux new -ADs "$selected" -c "$PROJECTS/$selected"
+    tmux new -ADs "$selected" -c "$PROJECTS_PATH/$selected"
 fi
