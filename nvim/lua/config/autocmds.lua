8f24c606c7da3c738355f2bb164d0603de942db3
Alucard <fire0lord0zuko@proton.me>
2026-06-24 00:02:18 +0300

    Update indentation in autocmds.lua

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -4,16 +4,15 @@
 local highlights = require("config.highlights")
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "*", "c", "cpp"},
-  callback = highlights.c_hl,
+    pattern = { "*", "c", "cpp"},
+    callback = highlights.c_hl,
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if client and client.server_capabilities.semanticTokensProvider then
-      client.server_capabilities.semanticTokensProvider = nil
-    end
-  end,
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client and client.server_capabilities.semanticTokensProvider then
+            client.server_capabilities.semanticTokensProvider = nil
+        end
+    end,
 })
-
